fa8a96f00cd5454bde4e6d8092e5912c2fec9d7d
MrBiscotto <floriangenicq@gmail.com>
2018-12-02 22:42:30 +0100

    Angular works !

--- GroupeA07/App_Start/WebApiConfig.cs
+++ GroupeA07/App_Start/WebApiConfig.cs
@@ -14,6 +14,13 @@ namespace GroupeA07
         public static void Register(HttpConfiguration config)
         {
 
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //config.MessageHandlers.Add(new TokenValidationHandler());
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
--- GroupeA07/Global.asax.cs
+++ GroupeA07/Global.asax.cs
@@ -14,13 +14,13 @@ namespace GroupeA07
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RegisterRoute(RouteTable.Routes);
+        }
 
-
+        private static void RegisterRoute(RouteCollection routes)
+        {
+            routes.MapRoute("catchAll", "{*catchAll}", new { controller = "Index", action = "Index" });
         }
     }
 }
